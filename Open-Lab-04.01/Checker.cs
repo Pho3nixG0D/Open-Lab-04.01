5d236c77dad54d8036006840036d534c9ad119d0
Oliver Krška <oliverkrska@gmail.com>
2022-09-21 19:14:39 +0200

    Update Checker.cs

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -1,12 +1,21 @@
-﻿using System;
-
 namespace Open_Lab_04._01
 {
     public class Checker
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i+1 < str.Length; i++)
+            {
+                int x = i + 1;
+                if (str[i] == str[x])
+                {
+                    return true;
+                }
+            };
+            return false;
+
+
+
         }
     }
 }
